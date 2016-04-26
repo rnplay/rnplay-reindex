eff4fed3e9de6293d6ea24358dedb0fc77ea3025
Joshua Sierles <joshua@diluvia.net>
2016-04-26 17:31:57 +0200

    allow more than one command for schema operations

--- bin/schema.js
+++ bin/schema.js
@@ -1,7 +1,8 @@
 import {spawnSync} from 'child_process';
+import process from 'process';
 
 import {REINDEX_URL, REINDEX_TOKEN} from '../src/config.js';
 
-let command = `node_modules/.bin/reindex -u ${REINDEX_URL} -t ${REINDEX_TOKEN} schema-relay data/schema.json`;
+let command = `node_modules/.bin/reindex -u ${REINDEX_URL} -t ${REINDEX_TOKEN} ${process.argv[2]} data/schema.json`;
 console.log(command)
 spawnSync('node', command.split(" "), {stdio: 'inherit'})
