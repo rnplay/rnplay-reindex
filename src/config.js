d7236be23f9a4f617cbc9dcba628819b7fb02813
Joshua Sierles <joshua@diluvia.net>
2016-09-12 13:25:01 +0200

    add auth0 dev client id/secret

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  AUTH0_CLIENT_ID: 'WOhfekiwZL8GtzMnAFWHzY6LFSEcjg7q',
+  AUTH0_CLIENT_SECRET: 'zauUfz8eKToEQHrfFgbqNR0ZWd6xvMKZcEq0zr2RkWay6BN1dhl0jESmIpZ9-rIo',
   REINDEX_URL: 'https://galvanized-manganese-321.myreindex.com',
   REINDEX_TOKEN: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc0FkbWluIjp0cnVlLCJpYXQiOjE0NjExNDcwNTR9.PqmKbS8l1cRr9iWO33b8WH3T6nIoELsF9Fhqq_cOoTo',
   WEBTASK_BASE_URL: 'https://webtask.it.auth0.com/api/run/wt-info-rnplay_org-0'
